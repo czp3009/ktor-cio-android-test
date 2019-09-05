dfd3859ba8a2f39f3a7c263175b435a1537f28b8
czp3009 <czp3009@gmail.com>
2019-09-05 13:49:29 +0800

    show headers

--- app/src/main/java/com/hiczp/myapplication/MainActivity.kt
+++ app/src/main/java/com/hiczp/myapplication/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.hiczp.myapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.*
-import com.google.gson.JsonArray
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.features.json.GsonSerializer
-import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.logging.LogLevel
 import io.ktor.client.features.logging.Logging
 import io.ktor.client.request.get
+import io.ktor.client.response.HttpResponse
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.delay
@@ -19,9 +18,6 @@ import kotlinx.coroutines.launch
 private val httpClient = HttpClient(CIO) {
     install(Logging) {
         level = LogLevel.ALL
-    }
-    install(JsonFeature) {
-        serializer = GsonSerializer()
     }
 }
 
@@ -36,25 +32,28 @@ class MainActivity : AppCompatActivity() {
         model.repos.observe(this) {
             textView.text = it
         }
+        model.times.observe(this) {
+            Toast.makeText(this, "$it times", Toast.LENGTH_SHORT).show()
+        }
         model.start()
     }
 }
 
 class MainViewModel : ViewModel() {
-    val repos = MutableLiveData<String>("Loading")
+    val repos = MutableLiveData("Loading")
+    val times = MutableLiveData(0)
 
     fun start() {
         viewModelScope.launch(IO) {
             while (true) {
                 repos.postValue("Loading...")
-                httpClient.get<JsonArray>("https://api.github.com/orgs/JetBrains/repos")
-                    .let { jsonArray ->
-                        repos.postValue(
-                            jsonArray.joinToString(separator = "\n") {
-                                it.asJsonObject["name"].asString
-                            }
-                        )
-                    }
+                val httpResponse = httpClient.get<HttpResponse>("https://api.github.com/orgs/JetBrains/repos")
+                repos.postValue(
+                        httpResponse.headers.entries().joinToString(separator = "\n") { (key, values) ->
+                            values.joinToString(separator = "\n") { "$key=$it" }
+                        }
+                )
+                times.postValue(times.value!! + 1)
                 delay(10_000)
             }
         }
